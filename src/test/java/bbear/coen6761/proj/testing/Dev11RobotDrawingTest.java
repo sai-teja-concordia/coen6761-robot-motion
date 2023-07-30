f86c2f8c7259f552e0272796ba1211c437dabd3f
Rushit Kanani <patelrushit2110@gmail.com>
2023-07-30 12:54:32 -0400

    Update Dev11RobotDrawingTest.java

--- src/test/java/bbear/coen6761/proj/testing/Dev11RobotDrawingTest.java
+++ src/test/java/bbear/coen6761/proj/testing/Dev11RobotDrawingTest.java
@@ -3,6 +3,7 @@ package bbear.coen6761.proj.testing;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import bbear.coen6761.proj.RobotDrawing;
@@ -10,6 +11,7 @@ import bbear.coen6761.proj.RobotDrawing;
 public class Dev11RobotDrawingTest {
 	private RobotDrawing robot = new RobotDrawing();
 	
+	@DisplayName("TC 2 User inputs <i> without defining size of floor")
 	@Test
 	public void initializationTest() {
 		robot.processCommand("i");
@@ -17,7 +19,8 @@ public class Dev11RobotDrawingTest {
                 + "The command and number should be separated by a space.\n";
 		assertEquals(expectedOutput, robot.getOutputArea().getText());
 	}
-
+	
+	@DisplayName("TC 3 User inputs <p> without initiliazing the system")
 	@Test
 	public void initializationTest1() {
 		robot.processCommand("p");
@@ -26,13 +29,14 @@ public class Dev11RobotDrawingTest {
 	assertEquals(expectedOutput, robot.getOutputArea().getText());
 	}
 
- 
+	@DisplayName("TC 4 User inputs <I 8> to initiliaze the array")
 	@Test
 	public void initializationTest2() {
 		robot.processCommand("i 8");
 		assertTrue(robot.isInitialized());
 	}
-	  
+	
+	@DisplayName("TC 1 User presses Enter key")
 	@Test
 	public void initializationTest3() {
 		robot.processCommand("");
@@ -40,7 +44,7 @@ public class Dev11RobotDrawingTest {
         		+ "the system using the 'i' command before executing any other commands.\n";
 		assertEquals(expectedOutput, robot.getOutputArea().getText());
 	}
-
+	@DisplayName("TC 6 User inputs <d> or <D> to keep pen down")
 	@Test
 	public void PenDownTest() {
 		initializeSystemAndAssert(10);
@@ -48,6 +52,7 @@ public class Dev11RobotDrawingTest {
 		assertTrue(robot.isPenDown());
 	}
 	
+	@DisplayName("TC 7 User inputs <u> or <U> to keep pen up")
 	@Test
 	public void PenUpTest() {
 		//robot.processCommand("i 10");
@@ -55,13 +60,35 @@ public class Dev11RobotDrawingTest {
 		robot.processCommand("u");
 		assertFalse(robot.isPenDown());
 	}
-
+	
+	@DisplayName("TC 5 User inputs <z> ")
 	@Test
 	public void invalidInputTest() {
 		robot.initializeSystem(10);
 		robot.processCommand("z");
 		String expected = "Error: Command not recognized.\n";
 		assertEquals(expected, robot.getOutputArea().getText());
+		
+	}
+	
+	@DisplayName("TC 8 From the Direction North, the User inputs <r> or <R> expected direction is East")
+	@Test
+	public void rotateRightTest() {
+		initializeSystemAndAssert(10);
+		robot.processCommand("r");
+		String expected = "E";
+		assertEquals(expected, robot.getDirection());
+		
+	}
+	
+	@DisplayName("TC 9 From the Direction West, the User inputs <l> or <L> expected direction is South")
+	@Test
+	public void rotateLeftTest() {
+		initializeSystemAndAssert(10);
+		robot.processCommand("l");
+		robot.processCommand("l");
+		String expected = "S";
+		assertEquals(expected, robot.getDirection());
 	}
 	
 	
